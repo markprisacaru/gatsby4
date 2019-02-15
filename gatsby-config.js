4fb38d59e1952ea4abf875f3e247c8182d6d4b9a
Mark Prisacaru <markprisacaru@gmail.com>
2019-02-15 12:50:26 -0600

    gatsby6

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,16 @@
 module.exports = {
     siteMetadata: {
-        title: `Pandas Eating Lots`,
-      },
+      title: `Pandas Eating Lots`,
+    },
     plugins: [
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-              name: `src`,
-              path: `${__dirname}/src/`,
-            },
-          },
+      {
+        resolve: `gatsby-source-filesystem`,
+        options: {
+          name: `src`,
+          path: `${__dirname}/src/`,
+        },
+      },
+      `gatsby-transformer-remark`,
       `gatsby-plugin-emotion`,
       {
         resolve: `gatsby-plugin-typography`,
